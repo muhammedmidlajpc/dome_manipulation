1a9b71d44b174bc7284884a5a33d7c35f454402f
Muhammed Midlaj PC <midlajpc531@gmail.com>
2024-09-04 15:35:13 +0530

    edit added

--- jq.js
+++ jq.js
@@ -1,11 +1,24 @@
+$(document).ready(() => {
+  const div = $("#container");
+  //   div.css('color','blue') //if it has only one style
 
-$(document).ready(()=>{
-
-  const div=$('#container')
-//   div.css('color','blue') //if it has only one style
-
-  div.css({'color':'blue','backgroundColor':'red'})
-  const h1=$('<h1>')
-  
-})
-
+  //   div.css({'color':'black','backgroundColor':'red'})
+  //   const h1=$('<h1>')
+  //   console.log(h1);
+  //   h1.text('new text')
+  //   div.append(h1)
+  // $(document.body).append(h1)
+  // div.remove()
+  const value = $("#input").val();
+  const newinput = $("<input>");
+  const save=$("<button>")
+  newinput.val(value)
+  save.text('save')
+  save.click(()=>{
+    const newvalue=newinput.val()
+    $('#input').val(newvalue)
+    save.remove()
+    newinput.remove()
+  })
+  div.append(newinput,save)
+});
--- todo.js
+++ todo.js
@@ -3,21 +3,37 @@ const btn = document.getElementById("btn");
 const ul = document.getElementById("ul");
 btn.addEventListener("click", () => {
   const li = document.createElement("li");
-  li.textContent=input.value
-  const rmv=document.createElement('button')
-  rmv.addEventListener("click",()=>{
-    li.remove()
-  
+  li.textContent = input.value;
+  const rmv = document.createElement("button");
+  rmv.addEventListener("click", () => {
+    li.remove();
+  });
+  const edit = document.createElement("button");
+
+  edit.addEventListener("click", () => {
+    rmv.remove()
+    edit.remove()
+    const newinput = document.createElement("input");
+    newinput.value = li.textContent;
+    li.textContent = "";
+    
+    const save = document.createElement("button");
+    save.textContent = "save";
+    li.append(newinput);
+    li.append(save);
+    save.addEventListener("click",()=>{
+      const newvalue=newinput.value
+      li.textContent=newvalue
+      li.append(edit,rmv)
     })
-    const edit=document.createElement('button')
-  edit.addEventListener("click",()=>{
-      li.edit()
-    edit.textContent="edit"
-    li.append(edit)
-  })
-  rmv.textContent="remove"
-  li.append(rmv)
+  });
+
+  edit.textContent = "edit";
+  li.append(edit);
+
+  rmv.textContent = "remove";
+  li.append(rmv);
 
   ul.append(li);
-  input.value=""
+  input.value = "";
 });
